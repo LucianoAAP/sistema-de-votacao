e655447c890e44d257653f208a676298ec541921
LucianoAAP <lucianoalmeidaap@gmail.com>
2023-10-21 01:10:35 -0300

    cria construtor padrao para pessoa

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -6,6 +6,10 @@ package com.betrybe.sistemadevotacao;
 public class Pessoa {
   private String nome;
   
+  public Pessoa() {
+    
+  }
+  
   public Pessoa(String nome) {
     this.nome = nome;
   }
